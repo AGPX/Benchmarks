a15415de31aa3bf8cb0e198bc5abe9e7ab225e7e
AGPX <agpxnet@yahoo.it>
2025-01-30 00:28:06 +0100

    Update run_oscar64.bat
    Reduced optimization level to avoid loop unrolling (unfortunately, cannot be disabled via code)

--- fill/run_oscar64.bat
+++ fill/run_oscar64.bat
@@ -2,6 +2,6 @@ if not exist output (
 	mkdir output
 )
 
-CALL oscar64 -O3 -xz fill.cpp -o=output/fill.prg
+CALL oscar64 -O1 -xz fill.cpp -o=output/fill.prg
 if %ERRORLEVEL% GEQ 1 EXIT /B 1
 CALL x64sc.exe output/fill.prg
